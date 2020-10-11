cadecd7a5a4a95097812114541b87c743b6853a3
Kelsea Fields <kelsea.fields@yahoo.com>
2020-10-10 21:56:47 -0700

    layout with response and jquery

--- main.js
+++ main.js
@@ -1,40 +1,73 @@
 const url = "https://api.openweathermap.org/data/2.5/weather?";
 const qParam = "q=";
-// don't forget "&" between parts
-const oneCall = "onecall?";
-const latUrl = "lat=";
-const lonUrl = "lon=";
-const excludeUrl = "exclude=";
 const apiKey = "&appid=2e4ef5fbcc2a67361873c717f682f59d";
-const uiIndex = "uvi?";
-const uiIndexCall = url + "uvi?" + "&" + latUrl + "&" + lonUrl + apiKey;
 
+$(document).ready(function () {
+  
+  $("#search").on("click", function(event){
+    
+    event.preventDefault(); 
+ 
+    var citySearch = $("#search-term").val();
+
+    function cityWeatherData() {
+     const queryURL = url + qParam + citySearch + apiKey;
+      console.log(event);
+      $.ajax({
+      url: queryURL,
+      method: "GET",
+      }).then(function (forecast) {
+      console.log(forecast);
+      console.log(forecast.name);
+      console.log(Math.floor(forecast.main.temp * 1.8 - 459.67) + " F");
+      console.log(forecast.main.humidity);
+      console.log(forecast.wind.speed);
+      console.log(forecast.coord.lat, forecast.coord.lon);
+      lat = forecast.coord.lat;
+      long = forecast.coord.lon
+      // console.log(forecast.weather[0].icon);
+        var ulEl = $(".cities-list");
+        var cityName = $("<li>").text(forecast.name);
+        ulEl.append(cityName);
+
+        var DayforecastDiv = $("#day-forecast");
+        var cityNameDisplay = $("<p>").text(forecast.name + "  date  " + "  icon");
+        DayforecastDiv.append(cityNameDisplay);
+        var temp = $("<p>").text("Temparature: " + Math.floor(forecast.main.temp * 1.8 - 459.67) + " F");
+        DayforecastDiv.append(temp);
+        var humidity = $("<p>").text("Humidity " + forecast.main.humidity + " %");
+        DayforecastDiv.append(humidity);
+        var windSpeed = $("<p>").text("Wind Speed " + forecast.wind.speed);
+        DayforecastDiv.append(windSpeed);
+    }); 
+    } cityWeatherData();
+
+    
+    
+    
+    
+    
+    
+    
+  })
+  
+})
+    // // function uvIndex(){
+    // //   const url2 = "http://api.openweathermap.org/data/2.5/"
+    // //   const latUrl = "lat=" + lat;
+    // //   const lonUrl = "lon=" + lon;
+    // //   const uiIndex = "uvi?";
+  
+    // //   const uvIndexCall = url2 + uiIndex + latUrl + "&" + lonUrl + apiKey;
+    // //   console.log(uvIndexCall);
+  
+    // }
+// function buildQueryUrl() {
+// don't forget "&" between parts
+// const oneCall = "onecall?";
+// const excludeUrl = "exclude=";
 // url + oneCall + latUrl + "47.608013&" + lonUrl + "-122.335167" + apiKey;
 
 // https://api.openweathermap.org/data/2.5/weather?q=&appid={API key}
 // http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
 // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-// ready document here
-
-// var citySearch = $("wherever search input is") .... on click of submit button
-
-// run Ajax call after adding query url with user variable
-
-// const queryURL = url + qParam + citySearch + apiKey;
-// testing API connection ***PASSED****
-const queryURL = url + qParam + "Seattle" + apiKey;
-$.ajax({
-  url: queryURL,
-  method: "GET",
-}).then(function (forecast) {
-  console.log(forecast);
-  console.log(forecast.name);
-  console.log(Math.floor(forecast.main.temp * 1.8 - 459.67));
-  console.log(forecast.main.humidity);
-  console.log(forecast.wind.speed);
-});
-console.log(this);
-
-// equation to convert kelvin to F = Kelvin * 1.8 - 459.67 before adding to html
-var y = $("search");
